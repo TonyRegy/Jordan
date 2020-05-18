c9857f617df3c1948d14a88ba29017a7ae4449c7
TonyRegy <tonyregy@gmail.com>
2020-05-18 17:12:10 +0530

    switchcase

---
+++ switchcase.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+isPartime=1
+isFulltime=2
+totalSalary=0
+empRatePerHr=20
+numWorkingDays=20
+for(( day=1; day<=$numWorkingDays; day++ ))
+do
+empCheck=$((RANDOM%3))
+case $empCheck in
+$isFulltime)
+empHrs=8
+;;
+$isPartime)
+empHrs=4
+;;
+*)
+empHrs=0
+;;
+esac
+salary=$(( $empHrs * $empRatePerHr))
+totalSalary=$(( $totalSalary + $salary))
+done
