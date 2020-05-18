51c4823d4052f835dcab43e2916fb7bd9e9bc577
TonyRegy <tonyregy@gmail.com>
2020-05-18 12:45:20 +0530

    prints hellowelcome

---
+++ helloworld.java
@@ -0,0 +1,16 @@
+
+public class helloworld{
+
+public static void main(String []args)
+{
+while(true){
+
+System.out.println("hello welcome");
+try {
+
+Thread.sleep(60000);}
+catch (Exception ex) {}
+
+}
+}
+}
