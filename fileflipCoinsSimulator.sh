889f884ee80b334db3ed4e2e689fefe34acc4908
TonyRegy <tonyregy@gmail.com>
2020-05-19 15:35:54 +0530

    coin flip simulator

---
+++ fileflipCoinsSimulator.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -x
+h=0
+t=0
+for((i=0;i<=15;i++))
+do
+	s=$((RANDOM%2))
+	if [ $s -eq 1 ]
+	then
+	o=$(($h + 1))
+	h=$o
+	elif [ $s -eq 0 ]
+	then
+	p=$(($t + 1))
+	t=$p
+	else
+	echo "invalid"
+	fi
+done
+echo "no;of heads =" $o
+echo "no;of tails =" $p
